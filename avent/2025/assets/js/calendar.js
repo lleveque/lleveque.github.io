3b8a7cf86b12409c023bc35fa945ba20dcd9a9c0
Laurent Lévêque <laurent.leveque@ecoco2.com>
2025-12-04 01:19:43 +0100

    [avent] Change background and tiles background on homepage

--- avent/2025/assets/js/calendar.js
+++ avent/2025/assets/js/calendar.js
@@ -1,10 +1,22 @@
-const today = new Date()
+fetch('calendar.json')
+    .then(response => response.json()) // Parse JSON
+    .then(data => buildCells(data)) // Work with JSON data
+    .catch(error => console.error('Error fetching JSON:', error));
 
-let upperBound = (today <= new Date("2025-12-24")) ? today.getDate() : 24;
+function buildCells(calendar)
+{
+  console.log(calendar)
+  const today = new Date()
 
-for (var i = 0; i < upperBound; i++) {
-  let cell = document.createElement("a");
-  cell.style.background = "#fff" // gradients[i][0] + "(" + gradients[i][2] + ", " + gradients[i][3] + ")";
-  cell.href = "./" + (i+1).toString().padStart(2,'0') + ".html" ;
-  document.getElementById("calendar").appendChild(cell);
+  let upperBound = (today <= new Date("2025-12-24")) ? today.getDate() : 24;
+
+  for (var i = 0; i < upperBound; i++)
+  {
+    let dayString = (i+1).toString().padStart(2,'0')
+    let cell = document.createElement("a");
+
+    cell.setAttribute("style", "background: url('./assets/png/" + dayString + "-" + calendar[i].shortName + ".png');");
+    cell.href = "./" + dayString + ".html" ;
+    document.getElementById("calendar").appendChild(cell);
+  }
 }
